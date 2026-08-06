5cf5a847ffb50c79e14aa405610d3708bf95d599
mylaroll <wby3tg@virginia.edu>
2026-08-06 15:51:11 -0400

    Imported React features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
@@ -19,6 +20,15 @@ const cardsData = [
 ];
 
 function App() {
+    const [apodData, setApodData] = useState(null);
+    useEffect(() => {
+        async function getApodData(file) {
+            let myObject = await fetch(file);
+            let myData = await myObject.json();
+            setApodData(myData);
+        }
+        getApodData("https://api.nasa.gov/planetary/apod?api_key=Jusq3dHNj6XS0j3ypUVa00QLYHwM7wb48MpMvAG3")
+    }, []);
   return (
       <>
           <Navbar />
